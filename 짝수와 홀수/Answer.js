c73347e6b204be3f48462dfd745fb87511d45ed6
jindoeun <g01056767919@gmail.com>
2021-09-29 23:57:12 +0900

    탭 사이즈 변경

--- 짝수와 홀수/Answer.js
+++ 짝수와 홀수/Answer.js
@@ -1,12 +1,13 @@
 function solution(num) {
-  return num % 2 == 0 ? 'Even' : 'Odd';
+	return num % 2 == 0 ? 'Even' : 'Odd';
 }
+
 
 // old
 function old(num) {
 	var answer;
 	num = parseInt(num);
-	
+
 	if(num == 0){ //0
 		answer = 'Even';
 	} else if(num%2 == 0){ //짝수
@@ -14,11 +15,12 @@ function old(num) {
 	} else{ //홀수
 		answer = 'Odd';
 	}
-	
+
 	return answer;
 }
 
+
 // other people
 function other1(num) {
-  return num % 2 ? 'Odd' : 'Even';
+	return num % 2 ? 'Odd' : 'Even';
 }
