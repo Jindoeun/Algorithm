1064a3f66780f33bca0b92ba27e75e1d9971efdf
jwu <sju02048@naver.com>
2021-12-12 19:05:51 +0900

    삭제: only old 폴더

--- old/문자열을_정수로_변환.js
+++
@@ -1,9 +0,0 @@
-function solution(s) {
-	var answer = 0;
-	
-	answer = parseInt(s);
-	
-	document.write(answer + '<br>');
-	document.write(typeof answer);
-	return answer;
-}
--- old/완주하지_못한_선수.js
+++
@@ -1,9 +0,0 @@
-function solution(participant, completion) {
-	var answer = participant.filter(function(e){
-		return (participant[e] == completion[e]);
-	});
-	document.write(answer);
-	return answer;
-}
-
-solution([1, 2, 3, 4, 5], [2, 4]);
